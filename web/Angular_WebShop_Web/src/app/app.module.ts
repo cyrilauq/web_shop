162bbb6b023d9c51b3e50222174d1a73d42fcc5d
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-10-22 15:53:51 +0200

    add some components

--- web/Angular_WebShop_Web/src/app/app.module.ts
+++ web/Angular_WebShop_Web/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './global/header/header.component';
 import { ProductItemComponent } from './components/product/product-item/product-item.component';
 import { ProductListComponent } from './components/product/product-list/product-list.component';
+import { NewsWrapperComponent } from './components/news/news-wrapper/news-wrapper.component';
+import { NewsItemComponent } from './components/news/news-item/news-item.component';
+import { AuthMenuComponent } from './global/auth-menu/auth-menu.component';
+import { ActionMenuComponent } from './global/action-menu/action-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ProductItemComponent,
-    ProductListComponent
+    ProductListComponent,
+    NewsWrapperComponent,
+    NewsItemComponent,
+    AuthMenuComponent,
+    ActionMenuComponent
   ],
   imports: [
     BrowserModule
