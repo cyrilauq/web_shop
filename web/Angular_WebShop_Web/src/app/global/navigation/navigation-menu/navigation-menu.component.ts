ddbb985d966860fae6aa39686e4b918af496fecd
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-10-24 22:46:32 +0200

    Add some items to menu, items are not currently displayed

--- web/Angular_WebShop_Web/src/app/global/navigation/navigation-menu/navigation-menu.component.ts
+++ web/Angular_WebShop_Web/src/app/global/navigation/navigation-menu/navigation-menu.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-menu.component.css']
 })
 export class NavigationMenuComponent {
-
+    menu = [
+        {
+            title: "Books",
+            sous_cat: [
+                "manga",
+                "romance",
+                "thriller"
+            ]
+        },
+        {
+            title: "Divertissement",
+            sous_cat: [
+                "video games",
+                "playstations"
+            ]
+        },
+        {
+            title: "garden",
+            sous_cat: [
+                "tools",
+                "decorations",
+                "games"
+            ]
+        }
+    ]
 }
