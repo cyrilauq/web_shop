545e5b41d418f23412236ef1d3b1adbb4d4778e7
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-10-25 18:34:46 +0200

    unit test of appearing / disapearing of header

--- web/Angular_WebShop_Web/src/app/global/header/header.component.spec.ts
+++ web/Angular_WebShop_Web/src/app/global/header/header.component.spec.ts
@@ -7,41 +7,66 @@ import { NavigationMenuComponent } from '../navigation/navigation-menu/navigatio
 import { NavigationItemComponent } from '../navigation/navigation-item/navigation-item.component';
 import { ShowDirective } from 'src/app/directives/show/show.directive';
 import { FormsModule, NG_VALUE_ACCESSOR, NgModel } from '@angular/forms';
-import { forwardRef } from '@angular/core';
+import { DebugElement, forwardRef } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-        declarations: [
-            HeaderComponent,
-            AuthMenuComponent,
-            ActionMenuComponent,
-            NavigationMenuComponent,
-            NavigationItemComponent,
-            ShowDirective,
-            NgModel
-        ],
-        // Put this import when testing component with ngmodel directive 
-        imports: [
-            FormsModule
-        ]
-    }).compileComponents();
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                HeaderComponent,
+                AuthMenuComponent,
+                ActionMenuComponent,
+                NavigationMenuComponent,
+                NavigationItemComponent,
+                ShowDirective,
+                NgModel
+            ],
+            // Put this import when testing component with ngmodel directive 
+            imports: [
+                FormsModule
+            ]
+        }).compileComponents();
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(HeaderComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('WebShop');
-  });
+    it('should render title', () => {
+        const fixture = TestBed.createComponent(HeaderComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('h1')?.textContent).toContain('WebShop');
+    });
+
+    
+    it('When header hidden and menu btn clicked then the menu is visible', () => {    
+        const element: DebugElement = fixture.debugElement.query(By.css('.menu_btn'));
+        element.triggerEventHandler('click');
+        fixture.detectChanges();
+    
+        expect(
+            fixture.debugElement.query(By.css('app-navigation-menu')).nativeElement.style.visibility
+        ).toEqual('visible');
+    });
+
+    
+    it('When header visible and menu btn clicked then the menu is hidden', () => {    
+        const element: DebugElement = fixture.debugElement.query(By.css('.menu_btn'));
+        element.triggerEventHandler('click');
+        fixture.detectChanges();
+        element.triggerEventHandler('click');
+        fixture.detectChanges();
+    
+        expect(
+            fixture.debugElement.query(By.css('app-navigation-menu')).nativeElement.style.visibility
+        ).toEqual('hidden');
+    });
 });
