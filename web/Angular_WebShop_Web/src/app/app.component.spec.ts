ed1da1889912507778e1f75baca33c1c0d37c340
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-10-25 14:57:58 +0200

    begin of test correction and implementatin

--- web/Angular_WebShop_Web/src/app/app.component.spec.ts
+++ web/Angular_WebShop_Web/src/app/app.component.spec.ts
@@ -17,11 +17,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Angular_WebShop_Web');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Angular_WebShop_Web app is running!');
-  });
 });
--- web/Angular_WebShop_Web/src/app/global/header/header.component.spec.ts
+++ web/Angular_WebShop_Web/src/app/global/header/header.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { AuthMenuComponent } from '../auth-menu/auth-menu.component';
+import { ActionMenuComponent } from '../action-menu/action-menu.component';
+import { NavigationMenuComponent } from '../navigation/navigation-menu/navigation-menu.component';
+import { NavigationItemComponent } from '../navigation/navigation-item/navigation-item.component';
+import { ShowDirective } from 'src/app/directives/show/show.directive';
+import { NgModel } from '@angular/forms';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,8 +14,16 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
-    });
+        declarations: [
+            HeaderComponent,
+            AuthMenuComponent,
+            ActionMenuComponent,
+            NavigationMenuComponent,
+            NavigationItemComponent,
+            ShowDirective,
+            NgModel
+        ]
+    }).compileComponents();
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +31,12 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('WebShop');
   });
 });
