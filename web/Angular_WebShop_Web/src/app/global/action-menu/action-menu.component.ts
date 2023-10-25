1769935fae144e022e6f3e56c1911827c1f5b0dd
Auquier Cyril <cyrilauquier@hotmail.fr>
2023-10-25 09:35:22 +0200

    add click event on cart and account icons

--- web/Angular_WebShop_Web/src/app/global/action-menu/action-menu.component.ts
+++ web/Angular_WebShop_Web/src/app/global/action-menu/action-menu.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./action-menu.component.css']
 })
 export class ActionMenuComponent {
-
+    onAccount() {
+        throw new Error("Method not implemented yet.")
+    }
+    
+    onCart() {
+        throw new Error("Method not implemented yet.")
+    }
 }
